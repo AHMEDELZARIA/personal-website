25bec047a29992fe2493ac24c6fe1efd2dbb7416
Ahmed Elzaria <aelzaria@gmail.com>
2024-06-12 22:46:03 -0400

    added custom cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,3 +150,27 @@ var typingEffect = new Typed(".typedText",{
   backSpeed : 80,
   backDelay : 2000
 })
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", e => {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 1000, fill: "forwards" });
+})
+
+window.addEventListener("mousedown", () => {
+    cursorOutline.classList.add("shrunk");
+})
+
+window.addEventListener("mouseup", () => {
+    cursorOutline.classList.remove("shrunk");
+})
